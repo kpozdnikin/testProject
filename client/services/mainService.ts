8e3696ff169441f6303d22be15737588c0392cb1
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2017-03-20 18:16:24 +0300

    Проблемы с typescript

--- client/services/mainService.ts
+++ client/services/mainService.ts
@@ -1,5 +1,9 @@
 import * as angular from 'angular';
 
+interface ProjectResult {
+    data: Object
+}
+
 export class MainService {
 
     static $inject = ['$http'];
@@ -7,10 +11,10 @@ export class MainService {
     constructor(private $http: angular.IHttpService) {}
 
     getNews(limit, skip) {
-        return this.$http.get('/api/news?limit=' + limit + '&skip=' + skip).then((response: any) => response.data);
+        return this.$http.get('/api/news?limit=' + limit + '&skip=' + skip).then((response: ProjectResult) => response.data);
     }
 
     getEvents(limit, skip) {
-        return this.$http.get('/api/events?limit=' + limit + '&skip=' + skip).then((response: any) => response.data);
+        return this.$http.get('/api/events?limit=' + limit + '&skip=' + skip).then((response: ProjectResult) => response.data);
     }
 }
